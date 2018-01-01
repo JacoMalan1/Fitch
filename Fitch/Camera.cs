5f6acaa357347303af3a21a90e932cbbfb4bceb1
Jaco Malan <jacomalan.12@gmail.com>
2018-01-01 15:28:37 +0200

    Update

---
+++ Fitch/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Fitch
+{
+    class Camera
+    {
+
+        public static void ApplyTransform(ref Player player, GameWindow window)
+        {
+
+            Matrix4 transform = Matrix4.Identity;
+            transform = Matrix4.Mult(transform, Matrix4.CreateTranslation((-player.Position.X + window.Width / 2) - player.Width, (-player.Position.Y + window.Height / 2) - player.Height, 0));
+
+            GL.MultMatrix(ref transform);
+
+        }
+
+    }
+}
