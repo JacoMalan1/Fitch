ef7da4b700d135ae60b191987756adc72680deaf
Jaco Malan <jacomalan.12@gmail.com>
2018-01-01 10:19:14 +0200

    * ContentPipe.cs: * penguin.png: Initial commit
    * Fitch.csproj: Update.
    
    * Game.cs: Added a draw call and a texture to go along with it.
    
    * Program.cs: Added some GameOptions.
    
    * World.cs:
    * Texture2D.cs:
    * SpriteBatch.cs: Initial commit.

--- Fitch/Program.cs
+++ Fitch/Program.cs
@@ -7,7 +7,7 @@ namespace Fitch
     {
         public static void Main(string[] args)
         {
-            GameWindow window = new GameWindow(800, 600);
+            GameWindow window = new GameWindow(800, 600, new OpenTK.Graphics.GraphicsMode(32, 8, 0, 0));
 
             Game game = new Game(window);
             window.Run(1.0 / 60.0);
---
+++ Fitch/World.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Fitch
+{
+    public class World
+    {
+        public enum BlockType
+        {
+            Air,
+            Solid
+        }
+
+        private float BlockSize;
+
+        public float blockSize { get { return BlockSize; }}
+
+        public World(float blocksize)
+        {
+
+            this.BlockSize = blocksize;
+
+        }
+    }
+}
